ec7459ef0ce43d3faee7fa4e63ecbc31c61b9775
jihanv <hedjmny@gmail.com>
2026-01-16 11:06:06 +0900

    Add a listener to handle websocket connection from the client to the server.

--- public/scripts.js
+++ public/scripts.js
@@ -1,2 +1,2 @@
-// creates a live connection from your browser (client) to your Socket.IO server i.e. connect to the Socket.IO server running at http://localhost:4000
+// Create a live connection from your browser (client) to your Socket.IO server i.e. connect to the Socket.IO server running at http://localhost:4000
 const socket = io("http://localhost:4000");
--- server.js
+++ server.js
@@ -10,3 +10,9 @@ const socketio = require("socket.io");
 
 //Socket.IO attaches itself to the server and basically says “If anyone requests a URL starting with /socket.io/, I will handle it.”
 const io = socketio(expressServer, {});
+
+//on is a regular javascript/node event listener
+//listen for the connect event, when a client connects, we get a socket, that we can then use for the callback function
+io.on("connect", (socket) => {
+  console.log(socket.id, " has joined our server.");
+});
