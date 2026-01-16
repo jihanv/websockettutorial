568e9206af05c97b95cb1c2790d2c0ae6149e83f
jihanv <hedjmny@gmail.com>
2026-01-16 10:59:02 +0900

    Create a live connection from your browser (client) to your Socket.IO server

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,2 @@
-console.log(io);
+// creates a live connection from your browser (client) to your Socket.IO server i.e. connect to the Socket.IO server running at http://localhost:4000
+const socket = io("http://localhost:4000");
